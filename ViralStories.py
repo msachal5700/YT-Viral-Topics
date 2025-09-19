08554b349a05d785302372e3c3dbb71bfdfb1d4d
Muhammad Sachal <106876496+msachal5700@users.noreply.github.com>
2025-09-19 19:42:55 +0500

    Update ViralStories.py

--- ViralStories.py
+++ ViralStories.py
@@ -3,7 +3,7 @@ import requests
 from datetime import datetime, timedelta
 
 # YouTube API Key
-API_KEY = "Enter your API Key here"
+API_KEY = "AIzaSyD5fbllaFTeKODpWWEIRGxWTaGT_uNSdHo"
 YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 YOUTUBE_VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos"
 YOUTUBE_CHANNEL_URL = "https://www.googleapis.com/youtube/v3/channels"
